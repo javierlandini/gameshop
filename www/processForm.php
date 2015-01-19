96688668b6a9fe4a2b76dd46b41f570a8e1bae5d
Javier M. Landini <javierlandini@gmail.com>
2015-01-18 23:47:27 -0300

    Updating processForm.

--- www/processForm.php
+++ www/processForm.php
@@ -1,8 +1,6 @@
 <?php
 //change this to false when in Prod
-$result = true;
-//number of seconds to wait
-$s = 5;
+$result = false;
 
 
 $email = $_POST['email'];
@@ -11,20 +9,21 @@ $question = $_POST['question'];
 
 if (!empty($email) && !empty($name) && !empty($question) && empty($_POST['honeypot'])) {
 	$from = $email;
-	$to = 'gameshop-rosario@hotmail.com';
+	//$to = 'gameshop-rosario@hotmail.com';
+	$to = 'javierlandini@gmail.com';
 
-	$headers = 'From: gameshop@gameshop-rosario.com.ar' . "\r\n" .
+	$headers = 'From: gameshop@game-shop.com.ar' . "\r\n" .
     'Reply-To: ' . $from . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 	$subject = 'Contacto desde sitio Web';
 	$body = "Nombre: $name\r\n";
+	$body .= "Mail: $from\r\n";
 	$body .= "Mensaje:\r\n$question";
 
 	//uncomment in Prod
-	//$result = mail($to, $subject, $body, $headers);
+	$result = mail($to, $subject, $body, $headers);
 }
 
 $response = array('success' => $result, 'data' => $_POST);
-sleep($s);
 echo json_encode($response);
 ?>
